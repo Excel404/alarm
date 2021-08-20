2b3f4aa8c44413f7a6d550237e63532ed951377a
Excel404 <65623910+Excel404@users.noreply.github.com>
2021-08-20 19:55:47 +0100

    2nd update

--- alarm.js
+++ alarm.js
@@ -1,79 +1,135 @@
-var alarmInterface= document.createElement('div');
-alarmInterface.setAttribute("class","alarm-interface");
-var alarmButton= document.createElement("button");
-alarmButton.setAttribute("class","alarm-button");
-alarmButton.innerHTML = "Create Alarm";
-alarmInterface.appendChild(alarmButton);
-document.body.appendChild(alarmInterface);
+let alarmInterface= document.getElementById('alarm-interface');
 
-var self= this;
-function alarm(){
-  self.hourLabel= document.createElement("span");
-  self.hourLabel.innerHTML= "<br>"+"<br>"+'Hour';
-  alarmInterface.appendChild(self.hourLabel);
-  self.inputHour= document.createElement("input");
-  self.inputHour.setAttribute("class","input-hour");
-  self.inputHour.setAttribute("type","text");
-  self.minuteLabel= document.createElement("span");
-  self.minuteLabel.innerHTML= "  "+"Min";
-  self.inputMinute=  document.createElement("input");
-  self.inputMinute.setAttribute("class","input-minute");
-  self.inputMinute.setAttribute("type","text");
+let alarmButton= document.getElementById("alarm-button");
+alarmInterface.style.width = window.screen.innerWidth;
+alarmInterface.style.height = window.screen.innerHeight;
+let hourElem = document.getElementById('input-hour');
+let minuteElem = document.getElementById('input-minute');
+let timeDivElem = document.getElementsByName('dayTime');
+
+function createAlarm(){
+  this.displayDiv = document.createElement("div");
+  this.displayDiv.setAttribute('id', 'displayDiv');
+  this.header = document.createElement("span");
+  this.header.innerHTML ='Upcoming Alarm'+ '<br>';
+  this.time = document.createElement("span");
+  this.cancel = document.createElement('button');
+  this.cancel.innerHTML = 'Delete';
+  this.cancel.onclick= ()=>{
+    alarmInterface.removeChild(this.displayDiv);
+  }
+  this.space = document.createElement('br');
+  this.snoozeBtn = document.createElement("button");
+  this.snoozeBtn.innerHTML = 'Snooze';
+  this.snoozeBtn.onclick = snoozeAlarm;
+  this.dismissBtn = document.createElement("button");
+  this.dismissBtn.innerHTML = 'Dismiss';
+  this.dismissBtn.onclick = dismissAlarm;
+  this.displayDiv.appendChild(this.header);
+  this.displayDiv.appendChild(this.time);
+  this.displayDiv.appendChild(this.cancel);
+  this.displayDiv.appendChild(this.space);
+  alarmInterface.appendChild(this.displayDiv);  
   
-  self.setButton= document.createElement("button");
-  self.setButton.setAttribute("class","set-button");
-  self.setButton.innerHTML= "Set Alarm";
-  
-  alarmInterface.appendChild(self.inputHour);
-  alarmInterface.appendChild(self.minuteLabel);
-  alarmInterface.appendChild(self.inputMinute);
-  alarmInterface.appendChild(self.setButton);
-  
-  function createAlarm(Hour,Minute){
-    self.setTime= new Date()
-    var Year = self.setTime.getFullYear();
-    var Month= self.setTime.getMonth();
-    var Hour= self.inputHour.value;
-    var Minute= self.inputMinute.value;
-    var date = self.setTime.getDate();
-    self.setTime1= new Date(Year,Month,date,Hour,Minute);
-    var currentTime= self.setTime.getTime();
-    var setTime2= self.setTime1.getTime();
-    var currentHour = self.setTime.getHours();
-    var currentMinute= self.setTime.getMinutes();
-    
-    function repeat(){
-      var timeRem = setTime2- currentTime;
-      console.log(Hour +":"+ Minute) 
-      var timeRem1 = Hour - currentHour;
-      var timeRem2 = Minute - currentMinute;
-      console.log(timeRem);
-      if (setTime2==currentTime ){
-        alarmRing();
+  function checkAlarm(hour,minute){
+    hour = parseInt(hourElem.value);
+    minute = parseInt(minuteElem.value);
+    let setTime = new Date();
+    var currentHour = setTime.getHours();
+    var currentMinute = setTime.getMinutes();
+    var currentTime = currentHour*3600 + currentMinute*60;
+    var setTime2 = hour*3600 + minute*60;
+    var timeRem = setTime2 - currentTime;
+    var timeRem1 = hour - currentHour;
+    var timeRem2 = minute - currentMinute;
+    if(hour!= 12){
+      if(timeDivElem[0].checked){
+      
+        this.time.innerHTML = hourElem.value +':'+ minuteElem.value+ timeDivElem[0].value+' ';      
+        if (setTime2==currentTime){
+          this.header.innerHTML ='Alarm'+ '<br>';
+          this.displayDiv.appendChild(this.dismissBtn);
+          this.displayDiv.appendChild(this.snoozeBtn);
+
+          alarmRing();
+        }
+        else{
+          alert(`Alarm set for ${timeRem1}hours ${timeRem2}Minutes from now`);
+          window.setTimeout(checkAlarm, Math.abs(timeRem)*1000);
+      
+        }
+      }else if(timeDivElem[1].checked){
+        this.time.innerHTML = hourElem.value +':'+ minuteElem.value+ timeDivElem[1].value+' ';
+        hour = parseInt(hourElem.value)+12;
+        setTime2 = hour*3600 + minute*60;
+        timeRem = setTime2 - currentTime;
+        timeRem1 = hour - currentHour;
+        timeRem2 = minute - currentMinute;
+        if (setTime2==currentTime){
+          this.header.innerHTML ='Alarm'+ '<br>';
+          this.displayDiv.appendChild(this.dismissBtn);
+          this.displayDiv.appendChild(this.snoozeBtn);
+
+          alarmRing();
+        }
+        else{
+          alert(`Alarm set for ${timeRem1}hours ${timeRem2}Minutes from now`);
+          window.setTimeout(checkAlarm, Math.abs(timeRem)*1000);
+      
+        }
       }
-      else{
-        alert(timeRem1 +"hours :"+ timeRem2 +"Minutes remaining");
-        window.setTimeout(alarmRing, Math.abs(timeRem));
-    
+    }else{
+      if(timeDivElem[0].checked){
+        this.time.innerHTML = hourElem.value +':'+ minuteElem.value+ timeDivElem[0].value+' ';
+        hour = 0;
+        setTime2 = hour*3600 + minute*60;
+        timeRem = setTime2 - currentTime;
+        timeRem1 = 24 - currentHour;
+        timeRem2 = minute - currentMinute;
+        if (setTime2==currentTime){
+          this.header.innerHTML ='Alarm'+ '<br>';
+          this.displayDiv.appendChild(this.dismissBtn);
+          this.displayDiv.appendChild(this.snoozeBtn);
+          alarmRing();
+        }
+        else{
+          alert(`Alarm set for ${timeRem1}hours ${timeRem2}Minutes from now`);
+          window.setTimeout(checkAlarm, Math.abs(timeRem)*1000);
+      
+        }
+        
+      }else if(timeDivElem[1].checked){
+        this.time.innerHTML = hourElem.value +':'+ minuteElem.value+ timeDivElem[0].value+' ';      
+        if (setTime2==currentTime){
+          this.header.innerHTML ='Alarm'+ '<br>';
+          this.displayDiv.appendChild(this.dismissBtn);
+          this.displayDiv.appendChild(this.snoozeBtn);
+
+          alarmRing();
+        }
+        else{
+          alert(`Alarm set for ${timeRem1}hours ${timeRem2}Minutes from now`);
+          window.setTimeout(checkAlarm, Math.abs(timeRem)*1000);
+      
+        }
       }
     }
-    repeat();
-   
+    
   }
+  checkAlarm();
   function alarmRing(){
-    alert("Wake up, its time");
-      var snooze= prompt("Do you want to snooze alarm:");
-      if(snooze!="no"){
-        var snoozeTime= prompt("Snooze time in seconds");
-        
-        window.setTimeout(alarmRing, snoozeTime*1000);
-      }else{
-        alert("Thank God you are awake")
-      }
+    
+    navigator.vibrate([800,200,800,200,800,200,800,200,800,300,800]);
+    //alert('wakeup');
+    
   }
-  
-  self.setButton.addEventListener("click",createAlarm);
-  
+  var snooze
+  function snoozeAlarm(){
+    console.log('snooze');
+    snooze = window.setInterval(alarmRing, 5000);
+  }
+  function dismissAlarm(){
+    console.log('dismiss');
+    window.clearInterval(snooze);
+  }
 }
-alarmButton.addEventListener("click",alarm);
-
